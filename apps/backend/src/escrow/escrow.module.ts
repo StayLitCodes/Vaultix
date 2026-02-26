98cd00ee06247bd873f3646b77e6035e60dce93b
David Ojo <victorojodavid@gmail.com>
2026-02-26 09:41:50 +0100

    remove duplicate escrow files from src/escrow

--- apps/backend/src/escrow/escrow.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EscrowController } from '../modules/escrow/controllers/escrow.controller';
-import { EscrowService } from '../modules/escrow/services/escrow.service';
-
-@Module({
-  controllers: [EscrowController],
-  providers: [EscrowService],
-})
-export class EscrowModule {}
